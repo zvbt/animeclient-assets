64e9476ed87ed0c2173b790cf9147d95186b7688
zvbt <zvbt@pm.me>
2023-04-14 09:34:50 +0200

    initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+VANTA.FOG({
+  el: "*",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.0,
+  minWidth: 200.0,
+  highlightColor: 0x0,
+  midtoneColor: 0x121212,
+  lowlightColor: 0x2986cc,
+  baseColor: 0x121212,
+});
