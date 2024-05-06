3fd87679c8388edc25ea4275aa6954b35d043ce6
zvbt <cyadine@skiff.com>
2024-05-06 20:04:28 +0200

    Initial commit

---
+++ src/app/changelogs/page.tsx
@@ -0,0 +1,32 @@
+'use client'
+import Image from "next/image";
+import Link from "next/link";
+import Navbar from "@/components/fr/navbar";
+
+export default function Home() {
+  return (
+    <main>
+<div className="ctp-mocha h-screen bg-ctp-mantle text-ctp-text flex justify-center">
+        <video 
+          src="/assets/videos/6.webm" 
+          className='fixed top-0 left-0 w-screen h-screen object-cover z-0'
+          autoPlay={true}
+          muted={true}
+          loop={true}
+        ></video>
+        <div className='w-full h-full bg-ctp-crust opacity-50 z-10 absolute'></div>
+        <div className='mt-5 z-50 justify-center items-center'>
+          <h1 className='text-3xl'>Changelogs</h1>
+          <ul className='mt-5 list-disc'>
+            <li>Reverted back to the old engine (way more stable)</li>
+            <li>Changed UI color palette (<a href="#" className="text-ctp-blue">Catpuccin</a>)</li>
+            <li>Fixed ads blocking</li>
+            <li>Disabled hardware acceleration (Enable screenshare in Discord)</li>
+            <li>Removed darkmode for Voiranime,Vostfree</li>
+            <li>Removed app size limit</li>
+          </ul>
+        </div>
+      </div>
+    </main>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      '3xl': '1920px',
+    },
+  },
+  plugins: [
+    require("@catppuccin/tailwindcss")({
+      prefix: "ctp",
+      defaultFlavour: "mocha",
+    }),
+  ],
+};
+export default config;
