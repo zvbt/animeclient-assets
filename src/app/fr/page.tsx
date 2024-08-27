79555a9eaff7746d7d45e22a5a2685a660c70812
zvbt <cyadine@pm.me>
2024-08-27 22:12:03 +0200

    Fixed size

--- src/app/fr/page.tsx
+++ src/app/fr/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <Navbar />
                 <div className='absolute block w-screen h-screen border-[8px] border-ctp-crust pointer-events-none z-50 3xl:opacity-0'></div>
                 <div className="fixed top-10 left-0 w-screen h-screen z-10 text-ctp-text 3xl:top-0">
-                    <webview src='./changelogs' style={{ width: '100%', height: '100%' }} className='px-[8px] 3xl:px-0' id='loader'></webview>
+                    <webview src='./changelogs' className='px-[8px] 3xl:px-0 w-full h-[94.7%]' id='loader'></webview>
                 </div>
             </div>
     </main>
