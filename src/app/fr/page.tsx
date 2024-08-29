87146ea08f09206a07685d3d9fda644c7f76b89d
zvbt <cyadine@pm.me>
2024-08-29 17:54:47 +0200

    Fix sizing

--- src/app/fr/page.tsx
+++ src/app/fr/page.tsx
@@ -9,9 +9,9 @@ export default function Home() {
 <div className="ctp-mocha relative h-screen bg-ctp-mantle">
                 <Navbar />
                 <div className='absolute block w-screen h-screen border-[8px] border-ctp-crust pointer-events-none z-50 3xl:opacity-0'></div>
-                <div className="fixed top-10 left-0 w-screen h-screen z-10 text-ctp-text 3xl:top-0">
-                    <webview src='./changelogs' className='px-[8px] 3xl:px-0 w-full h-[94.7%]' id='loader'></webview>
-                </div>
+                <div className="flex top-10 left-0 w-screen h-screen z-10 text-ctp-text 3xl:top-0">
+                    <webview src='./changelogs' className='px-[8px] 3xl:px-0 w-full h-full pt-[44px] pb-[8px] 3xl:p-0' id='loader'></webview>
+                </div> 
             </div>
     </main>
   );
